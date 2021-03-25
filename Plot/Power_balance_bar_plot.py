01261b090c7ddbd489bddfa5f20e921ff7b837d9
maxcurie1996 <47543965+maxcurie1996@users.noreply.github.com>
2021-03-25 16:07:13 -0500

    Update

--- Plot/Power_balance_bar_plot.py
+++ Plot/Power_balance_bar_plot.py
@@ -1,15 +1,16 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 
 #https://matplotlib.org/3.3.3/api/_as_gen/matplotlib.pyplot.bar.html
 width=0.5
-labels = ['MTM', 'ETG']#, 'Neoclassical']#, 'Other'
+labels = ['MTM', 'ETG', 'Neoclassical']#, 'Other'
 #colors=['orange','blue']
-sizes = [2.7, 0.17]#, 0]
-error = [0.5,0.01]#,0]
-total_heating=4.2
-neo=total_heating-np.sum(sizes)
+sizes = [2.7, 0.17, 1.54]
+error = [0.5,0.01,0]
+total_trans=4.2
+trans_error=1.5
 
 locations=np.arange(0.5*width,0.5*width+float(len(labels))*width+0.00001,width)
 
@@ -20,6 +21,9 @@ for i in error:
 
 total_error=np.sqrt(total_error)
 
+print("Total transport: "+str(total))
+print("Total transport error: "+str(total_error))
+
 plt.clf()
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 for i in range(len(sizes)):
@@ -27,7 +31,15 @@ for i in range(len(sizes)):
 
 plt.bar(locations[-1], total, width, yerr=total_error,label='total')
 
-plt.axhline(total_heating,color='red',alpha=0.5,label='total heating')
+'''
+x_max=np.max(locations+0.5*width)
+x=[0,0,x_max,x_max]
+y=[total_trans-trans_error,total_trans+trans_error,total_trans+trans_error,total_trans-trans_error]
+matplotlib.patches.Rectangle((0,total_trans-trans_error),10,2.*trans_error,alpha=0.4)
+plt.fill(x,y,alpha=0.6,label='total transport')
+'''
+
+plt.axhline(total_trans,color='red',alpha=0.5,label='total heating')
 
 plt.legend()
 
