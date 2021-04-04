db4677da4b6a861b8dffac5c0fe06d5a5e6421b6
maxcurie1996 <47543965+maxcurie1996@users.noreply.github.com>
2021-04-04 14:10:46 -0500

    More automatic EV plot

--- Plot/LL_k_plot/EV_overplot_EV_plot.py
+++ Plot/LL_k_plot/EV_overplot_EV_plot.py
@@ -7,14 +7,12 @@ import array as arr
 
 name_list=['kx000','kx020','kx040','kx060','kx080']
 title_list=['kx=0.00','kx=0.20','kx=0.40','kx=0.60','kx=0.80']
-
+Dir_name='./1966ms/'
 #all the ky, so that plot can show the stable ky
-ky0=[0.05,0.06,0.07,0.08,0.11,0.13,0.14,0.16,0.18,0.1, 0.12, 0.15, 0.17, 0.18, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1]
-#i_list: 
-i_list=[2,3,4] 	#The kx folder has unstable ITG 
-				#where 0 is kx000, 1 is kx020 and so on. 
+ky0=[0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+Dopppler_kHz=245.6 #Doppler shift in kHz for ky=1.0
 
-#plt.clf()
+#Plot growth rate 
 
 fig, axs = plt.subplots(len(name_list), sharex=True, sharey=True, )
 fig.suptitle('Local linear k dependence')
@@ -22,13 +20,23 @@ fig.suptitle('Local linear k dependence')
 
 for i in range(len(name_list)):
 	name=name_list[i]
-	path='./EV_scan/'+name+'/'
+	path=Dir_name+name+'/'
 
 	MTM=pd.read_csv(path+'MTM.csv')  
 	ETG=pd.read_csv(path+'ETG.csv') 
-	
+	ITG=pd.read_csv(path+'ITG.csv') 
+	KBM=pd.read_csv(path+'KBM.csv') 
+
+
 	ky_min_list0=np.append(ETG['kymin'],MTM['kymin'])
+	ky_min_list0=np.append(ky_min_list0,ITG['kymin'])
+	ky_min_list0=np.append(ky_min_list0,KBM['kymin'])
+
+	print(str(ky_min_list0))
+	#clear the nan in the list
+	ky_min_list0 = [x for x in ky_min_list0 if str(x) != 'nan']
 	ky_min_list0=list(set(ky_min_list0))
+	print(str(ky_min_list0))
 	ky_min_list=[]
 	for ky_temp in ky_min_list0:
 		ky_min_list.append(round(ky_temp,2))
@@ -41,29 +49,31 @@ for i in range(len(name_list)):
 		print(k_temp)
 		ky.remove(k_temp)
 	print('*******'+str(ky))
-	
-
-	axs[i].scatter(ETG['kymin'],ETG['gamma(cs/a)'],marker='o',label='ETG',color='blue')
-	
-	axs[i].scatter(MTM['kymin'],MTM['gamma(cs/a)'],marker='o',color='red',label='MTM')
 
 	axs[i].scatter(ky,[0]*len(ky),marker='o',color='grey',label='Stable')
-	if i in i_list:
-		ITG=pd.read_csv(path+'ITG.csv') 
+
+	ITG_list0 = [x for x in ITG['kymin'] if str(x) != 'nan']
+	if len(ITG_list0)!=0:
 		axs[i].scatter(ITG['kymin'],ITG['gamma(cs/a)'],marker='o',color='green',label='ITG')
+	ETG_list0 = [x for x in ETG['kymin'] if str(x) != 'nan']
+	if len(ETG_list0)!=0:
+		axs[i].scatter(ETG['kymin'],ETG['gamma(cs/a)'],marker='o',color='blue',label='ETG')
+	MTM_list0 = [x for x in MTM['kymin'] if str(x) != 'nan']
+	if len(MTM_list0)!=0:
+		axs[i].scatter(MTM['kymin'],MTM['gamma(cs/a)'],marker='o',color='red',label='MTM')
+	KBM_list0 = [x for x in KBM['kymin'] if str(x) != 'nan']
+	if len(KBM_list0)!=0:
+		axs[i].scatter(KBM['kymin'],KBM['gamma(cs/a)'],marker='o',color='purple',label='KBM')
 	
 	plt.xlim(0.08,1)
 	plt.ylim(0,10)
 	axs[i].legend()
 
 	axs[i].set_title(title_list[i])
-#
-'''
-	if i==0:
-		plt.ylabel(r'$\gamma(cs/a)$',fontsize=10)
-	elif i==len(name_list)-1:
-		plt.xlabel(r'$k_y\rho_i$',fontsize=10)
-'''
+
+
+
+#Plot frequency in plasma frame
 
 plt.xlabel(r'$k_y\rho_s$',fontsize=10)
 plt.ylabel(r'$\gamma(cs/a)$',fontsize=10)
@@ -72,22 +82,73 @@ plt.show()
 
 
 fig, axs = plt.subplots(len(name_list), sharex=True, sharey=True, )
-fig.suptitle('Local linear k dependence')
-
+fig.suptitle('Local linear k dependence(Plasma frame)')
 
 for i in range(len(name_list)):
 	name=name_list[i]
-	path='./EV_scan/'+name+'/'
+	path=Dir_name+name+'/'
 
 	MTM=pd.read_csv(path+'MTM.csv')  
 	ETG=pd.read_csv(path+'ETG.csv') 
-	
+	ITG=pd.read_csv(path+'ITG.csv') 
+	KBM=pd.read_csv(path+'KBM.csv') 
+
 	ky_min_list0=np.append(ETG['kymin'],MTM['kymin'])
+	ky_min_list0=np.append(ky_min_list0,ITG['kymin'])
+	ky_min_list0=np.append(ky_min_list0,KBM['kymin'])
+
+	print(str(ky_min_list0))
+	#clear the nan in the list
+	ky_min_list0 = [x for x in ky_min_list0 if str(x) != 'nan']
 	ky_min_list0=list(set(ky_min_list0))
+	print(str(ky_min_list0))
 	ky_min_list=[]
 	for ky_temp in ky_min_list0:
 		ky_min_list.append(round(ky_temp,2))
 
+	axs[i].scatter(ky,[0]*len(ky),marker='o',color='grey',label='Stable')
+
+	ITG_list0 = [x for x in ITG['kymin'] if str(x) != 'nan']
+	if len(ITG_list0)!=0:
+		axs[i].scatter(ITG['kymin'],ITG['gamma(cs/a)'],marker='o',color='green',label='ITG')
+	ETG_list0 = [x for x in ETG['kymin'] if str(x) != 'nan']
+	if len(ETG_list0)!=0:
+		axs[i].scatter(ETG['kymin'],ETG['gamma(cs/a)'],marker='o',color='blue',label='ETG')
+	MTM_list0 = [x for x in MTM['kymin'] if str(x) != 'nan']
+	if len(MTM_list0)!=0:
+		axs[i].scatter(MTM['kymin'],MTM['gamma(cs/a)'],marker='o',color='red',label='MTM')
+	KBM_list0 = [x for x in KBM['kymin'] if str(x) != 'nan']
+	if len(KBM_list0)!=0:
+		axs[i].scatter(KBM['kymin'],KBM['gamma(cs/a)'],marker='o',color='purple',label='KBM')
+	
+	plt.xlim(0.08,1)
+	plt.ylim(0,1000)
+	axs[i].legend()
+	axs[i].set_title(title_list[i])
+plt.xlabel(r'$k_y\rho_s$',fontsize=10)
+plt.ylabel('Frequency(kHz)',fontsize=10)
+plt.show()
+
+#Plot frequency in lab frame
+
+fig, axs = plt.subplots(len(name_list), sharex=True, sharey=True, )
+fig.suptitle('Local linear k dependence(Lab frame)')
+for i in range(len(name_list)):
+	name=name_list[i]
+	path=Dir_name+name+'/'
+
+	ky_min_list0=np.append(ETG['kymin'],MTM['kymin'])
+	ky_min_list0=np.append(ky_min_list0,ITG['kymin'])
+	ky_min_list0=np.append(ky_min_list0,KBM['kymin'])
+
+	print(str(ky_min_list0))
+	#clear the nan in the list
+	ky_min_list0 = [x for x in ky_min_list0 if str(x) != 'nan']
+	ky_min_list0=list(set(ky_min_list0))
+	print(str(ky_min_list0))
+	ky_min_list=[]
+	for ky_temp in ky_min_list0:
+		ky_min_list.append(round(ky_temp,2))
 	ky=list(set(ky0))
 	print('*******'+str(ky)) 
 	#ky = arr.array('f', ky)
@@ -96,31 +157,27 @@ for i in range(len(name_list)):
 		print(k_temp)
 		ky.remove(k_temp)
 	print('*******'+str(ky))
-	
-
-	axs[i].scatter(ETG['kymin'],abs(ETG['omega(kHz)']),marker='o',label='ETG',color='blue')
-	
-	axs[i].scatter(MTM['kymin'],abs(MTM['omega(kHz)']),marker='o',color='red',label='MTM')
-
 	axs[i].scatter(ky,[0]*len(ky),marker='o',color='grey',label='Stable')
-	if i in i_list:
-		ITG=pd.read_csv(path+'ITG.csv') 
-		axs[i].scatter(ITG['kymin'],abs(ITG['omega(kHz)']),marker='o',color='green',label='ITG')
+
+	ITG_list0 = [x for x in ITG['kymin'] if str(x) != 'nan']
+	if len(ITG_list0)!=0:
+		axs[i].scatter(ITG['kymin'],ITG['gamma(cs/a)'],marker='o',color='green',label='ITG')
+	ETG_list0 = [x for x in ETG['kymin'] if str(x) != 'nan']
+	if len(ETG_list0)!=0:
+		axs[i].scatter(ETG['kymin'],ETG['gamma(cs/a)'],marker='o',color='blue',label='ETG')
+	MTM_list0 = [x for x in MTM['kymin'] if str(x) != 'nan']
+	if len(MTM_list0)!=0:
+		axs[i].scatter(MTM['kymin'],MTM['gamma(cs/a)'],marker='o',color='red',label='MTM')
+	KBM_list0 = [x for x in KBM['kymin'] if str(x) != 'nan']
+	if len(KBM_list0)!=0:
+		axs[i].scatter(KBM['kymin'],KBM['gamma(cs/a)'],marker='o',color='purple',label='KBM')
+	
 	
 	plt.xlim(0.08,1)
 	plt.ylim(0,1000)
 	axs[i].legend()
-
 	axs[i].set_title(title_list[i])
-#
-'''
-	if i==0:
-		plt.ylabel(r'$\gamma(cs/a)$',fontsize=10)
-	elif i==len(name_list)-1:
-		plt.xlabel(r'$k_y\rho_i$',fontsize=10)
-'''
-
 plt.xlabel(r'$k_y\rho_s$',fontsize=10)
 plt.ylabel('Frequency(kHz)',fontsize=10)
-
 plt.show()
+
